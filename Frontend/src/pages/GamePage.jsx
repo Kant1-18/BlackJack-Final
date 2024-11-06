3cab5a227db7e13a19288423465c1d542f1e00c7
Titinite <thibault.leray@supdevinci-edu.fr>
2024-11-06 09:48:05 +0100

    Page de la partie

--- Frontend/src/pages/GamePage.jsx
+++ Frontend/src/pages/GamePage.jsx
@@ -1,5 +1,6 @@
 import PlayerTable from "../components/PlayerTable";
 import { useState } from "react";
+import "./GamePage.css";
 
 export default function GamePage() {
   const [players, setPlayers] = useState([
@@ -10,6 +11,29 @@ export default function GamePage() {
   return (
     <div>
       <h1>Game Page</h1>
+      <h3>C'est au tour de Player 1</h3>
+
+      <fieldset>
+        <legend>Choisir le nombre de dés à lancer</legend>
+
+        <div>
+          <input type="radio" id="de1" name="choix_dé" value="1" />
+          <label for="de1">1</label>
+        </div>
+        <div>
+          <input type="radio" id="de2" name="choix_dé" value="2" />
+          <label for="de2">2</label>
+        </div>
+        <div>
+          <input type="radio" id="de3" name="choix_dé" value="3" />
+          <label for="de3">3</label>
+        </div>
+
+      </fieldset>
+
+      <button>Lancer les dés</button>
+      <button>Finir le tour</button>
+
       <PlayerTable players={players} />
     </div>
   );
